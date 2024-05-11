2ba541bd337af9be11e53b06b9857535a9443b9b
kycbulante <92903243+kycbulante@users.noreply.github.com>
2024-05-11 23:21:00 +0800

    Update admintry.php
    removed absent checker

--- ADMINNEW/admintry.php
+++ ADMINNEW/admintry.php
@@ -123,51 +123,6 @@ $currtime = date("F d, Y", $timeconv);
 $currdate = date("Y-m-d", $timeconv);
 $curryear = date("Y", $timeconv);
 
-if (date('N') < 6) {
-$sql = "SELECT emp_id FROM employees
-        WHERE NOT EXISTS (
-            SELECT 1 FROM time_keeping
-            WHERE employees.emp_id = time_keeping.emp_id
-            AND DATE(time_keeping.timekeep_day) = CURDATE()
-        )";
-
-$result = $conn->query($sql);
-if (!$result) {
-  echo "<script>Error executing query: </script>" . $conn->error;
-  // handle the error, e.g., return or exit
-}
-// Iterate through the employees without a timekeeping record
-while ($row = $result->fetch_assoc()) {
-    $employee_id = $row['emp_id'];
-
-    // Check if an absence record already exists for today
-    $check_existing_sql = "SELECT 1 FROM absences
-                           WHERE emp_id = $employee_id
-                           AND absence_date = CURDATE()";
-
-    $existing_result = $conn->query($check_existing_sql);
-
-    if ($existing_result === false) {
-      echo "Error executing query: " . $conn->error;
-      // handle the error, e.g., return or exit
-  }
-    // If no absence record exists, insert a new record
-    if (mysqli_num_rows($existing_result) == 0) {
-        $insert_sql = "INSERT INTO absences (emp_id, absence_date)
-                       VALUES ($employee_id, CURDATE())";
-
-        if ($conn->query($insert_sql) === TRUE) {
-            echo "<script> Record inserted successfully for employee with ID $employee_id.\n</script>";
-        } else {
-            echo "<script>Error inserting record: " . $conn->error . "\n </script>";
-        }
-    } else {
-        echo "<script>Absence record already exists for employee with ID $employee_id.\n </script>";
-    }
-}
-} else {
-  echo "<script>Current date is a weekend. No action taken.</script>";
-}
 
 $checkpperiod = "SELECT pperiod_range FROM payperiods WHERE CURDATE() BETWEEN pperiod_start and pperiod_end";
 $checkpperiodexec = mysqli_query($conn, $checkpperiod) or die("FAILED TO CHECK PAYPERIOD " . mysqli_error($conn));
@@ -853,7 +808,7 @@ $currentDate = date('F j, Y');
         CURRENT PAYROLL PERIOD
       </div>
 <div class="card-body d-flex justify-content-center ">
-  <h6 class="text-center">                  
+  <h6 class="text-center"> 
   <?php
 if (isset($currpperiod)) {
     // Extracting start and end dates from the current period
