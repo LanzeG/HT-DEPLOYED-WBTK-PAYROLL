f3b40e9ca1713351d1a7fd6d38eced8159379514
kycbulante <92903243+kycbulante@users.noreply.github.com>
2024-05-12 00:53:16 +0800

    Update printpayslip.php

--- EMPLOYEENEW/printpayslip.php
+++ EMPLOYEENEW/printpayslip.php
@@ -173,7 +173,7 @@ if ($period_start !== null && $period_end !== null) {
     </style>
     </head>
     <body>
-     <h4 style="text-align:center; margin-bottom:0px;" class="text-primary">WEB-BASED TIMEKEEPING AND PAYROLL SYSTEM USING FINGERPRINT BIOMETRICS</h4>
+     <h4 style="text-align:center; margin-bottom:0px;" class="text-primary">PAYSLIP</h4>
         <p style="text-align:center;  margin-top:0px; margin-bottom:10px;">For the period: '.$formattedDateRange.'</p>
         <table>
         <tr>
@@ -184,7 +184,7 @@ if ($period_start !== null && $period_end !== null) {
         <tr>
             <td>Name: ' . $name . '</td>
             <td></td>
-            <td>Pay Period: ' . $name . '</td>
+            <td>Pay Period: ' . $payperiod . '</td>
         </tr>
         <tr>
             <td>Department: ' . $dept . ' </td>
